c1428e6b43478169b785ee324f62ed0cc4e01a92
basyura <basyura@gmail.com>
2022-08-29 17:58:43 +0900

    markdown-it

--- inkdrop-client/src/components/Note.js
+++ inkdrop-client/src/components/Note.js
@@ -1,12 +1,20 @@
 import { currentNoteAtom } from "../atoms/Atoms";
 import { useAtomValue } from "jotai";
+import MarkdownIt from "markdown-it";
 
 const Note = () => {
   const currentNote = useAtomValue(currentNoteAtom);
+  const md = new MarkdownIt();
+  if (currentNote.title == null) {
+    return <div></div>;
+  }
+  const RenderHTML = (props) => (
+    <div dangerouslySetInnerHTML={{ __html: props.HTML }}></div>
+  );
   return (
     <div>
       <h1> {currentNote.title}</h1>
-      <div> {currentNote.body} </div>
+      <RenderHTML HTML={md.render(currentNote.body)} />
     </div>
   );
 };
